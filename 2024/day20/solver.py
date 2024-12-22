12727ab2b967cb2444e0ea86b5fb5a60d397b6a0
alfred <alfredronning@gmail.com>
2024-12-22 18:06:55 +0100

    20

--- 2024/day20/solver.py
+++ 2024/day20/solver.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 alver = open("usorterte_alver.txt").read().strip().split("\n")
-alver = [(alv.split()[0], int(alv.split()[1])) for alv in alver]+[("pad", float("inf"))]*5
+alver = [(alv.split()[0], int(alv.split()[1])) for alv in alver]
 fester = defaultdict(int)
 
 i = 0
@@ -13,18 +13,24 @@ while i < len(alver)-1:
         flyttet += 1
         if flyttet == 7:
             flyttet = 0
-            alver[i+1], alver[i+5] = alver[i+5], alver[i+1]
-            alver[i+2], alver[i+4] = alver[i+4], alver[i+2]
-            fester[alver[i+1]] += 1
-            fester[alver[i+2]] += 1
-            fester[alver[i+3]] += 1
-            fester[alver[i+4]] += 1
-            fester[alver[i+5]] += 1
+            try:
+                alver[i+1], alver[i+5] = alver[i+5], alver[i+1]
+                alver[i+2], alver[i+4] = alver[i+4], alver[i+2]
+                fester[alver[i+1]] += 1
+                fester[alver[i+2]] += 1
+                fester[alver[i+3]] += 1
+                fester[alver[i+4]] += 1
+                fester[alver[i+5]] += 1
+            except:
+                alver[i+1:i+6] = alver[i+1:i+6][::-1]
+                for alv in alver[i+1:i+6]:
+                    fester[alv] += 1
         i -= 1
     else:
         i += 1
 
 festealv = max(fester, key=lambda x: fester[x])
 print(festealv[0]+","+str(fester[festealv]))
+print(alver.index(festealv))
 
 
